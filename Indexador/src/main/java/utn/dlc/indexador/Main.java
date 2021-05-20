4b99e09ab502e02952be28149e3733d61649e99d
FEDERICO HERNANDEZ <frhernandez22@gmail.com>
2021-05-20 03:21:16 -0300

    Merge origin/master
    Conflicts:
    	src/main/webapp/index.html

--- Indexador/src/main/java/utn/dlc/indexador/Main.java
+++ Indexador/src/main/java/utn/dlc/indexador/Main.java
@@ -35,6 +35,13 @@ public class Main {
         }
     }
     
+    public static int fibo(int n)
+    {
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        System.out.println("Fibo("+n+")");
+        return fibo(n-1) + fibo(n-2);
+    }
     /*
     @Inject PruebaInject prueba;
     
